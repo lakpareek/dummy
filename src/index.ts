1bd632860a13a099998e66c724a6474281f22bd8
Rishi-0007 <rishikumarnayak9@gmail.com>
2024-09-17 16:43:49 +0530

    middleware edited

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
 });
 
 // Define the User interface
